92b34fbab83523dca0b3f94fcde9c2991fab60f3
Bhargav Maniar <bhargav.mahendrabhai2013@vit.ac.in>
2017-03-24 15:45:42 +0530

    Thunk Commit

--- client/app.js
+++ client/app.js
@@ -1,22 +1,15 @@
-// Code Here
-import React from "react";
-import ReactDOM from "react-dom";
-import { Router, Route, IndexRoute, browserHistory } from "react-router";
-import AlbumComponent from "./components/Album.component";
-import PhotoComponent from "./components/Photo.component";
-import Main from "./components/Main.component";
-import { Provider } from "react-redux";
-import store from "./store";
-import App from './components/MainScript'
-import css from './styles/style.styl';
-var router = (
-  <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={AlbumComponent} />
-        <Route path="/view/:id" component={PhotoComponent} />
-      </Route>
-    </Router>
-  </Provider>
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+
+import ItemList from './components/ItemList';
+
+const store = configureStore();
+
+render(
+    <Provider store={store}>
+        <ItemList />
+    </Provider>,
+    document.getElementById('root')
 );
-ReactDOM.render(router, document.getElementById("root"));
--- client/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import posts from './postReducer';
-import comments from './commentReducer';
-
-var rootReducer = combineReducers({
-    posts,
-    comments
-})
-export default rootReducer;
