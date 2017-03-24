ed6924a886d712e1fd116e5cd6e32f7c3e583ce0
Bhargav Maniar <bhargav.mahendrabhai2013@vit.ac.in>
2017-03-24 16:16:55 +0530

    Component Structures

--- client/app.js
+++ client/app.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-
-import ItemList from './components/ItemList';
-
+import App from './components/MainScript'
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+import {baseUrl} from './components/common-components/common';
 const store = configureStore();
-
+//here sending empty initialstates
 render(
     <Provider store={store}>
-        <ItemList />
+        <Router history={browserHistory}>
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
     </Provider>,
     document.getElementById('root')
 );
---
+++ client/components/common-components/common.js
@@ -0,0 +1,1 @@
+exports.baseUrl = "http://10.203.60.100:8000";
