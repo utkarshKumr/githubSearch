a99d5b6c94d200bfe885bc3bba3698df04d8224b
Utkarsh Kumar 2 <ukuma8@sapient.com>
2017-03-27 10:28:42 +0530

    item

--- client/components/Main.component.js
+++ client/components/Main.component.js
@@ -1,8 +1,12 @@
 import React from 'react';
 class Main extends React.Component{
+    componentDidMount(){
+        this.props.fetchData()
+    }
     render(){
         return (<div>
         <h1>Using Routes</h1>
+
         {/*{React.cloneElement(this.props.children,this.props)}*/}
         </div>)
     }
