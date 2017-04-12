8c9cde1ca5de505c158bdf82cd648cbd3e72b82a
Sibasis Mohanty <smoh34@sapient.com>
2017-04-12 15:56:36 +0530

    chart responsive

--- client/components/TraderDesktop/ChartComponent.js
+++ client/components/TraderDesktop/ChartComponent.js
@@ -1,20 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { WindowResizeListener } from 'react-window-resize-listener';
 
 var BarStackHorizontalChart = require('react-d3-basic').BarStackHorizontalChart;
+var Legend = require('react-d3-core').Legend;
 
 class ChartComponent extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.width=1000;
+        this.showLegend;
+    }
     componentDidMount(){
         this.props.getOrders("http://localhost:8080/orders");
     }
 
 
+    changeWidth(windowSize){
+        if (windowSize.windowWidth <= 768 ) {
+            this.width = windowSize.windowWidth;
+            this.showLegend = true;
+        }
+        else if(windowSize.windowWidth < 998){
+            this.width = windowSize.windowWidth-150;
+            if(windowSize.windowWidth <= 991)
+                this.showLegend = true;
+            else
+                this.showLegend = false;
+        }
+        else {
+            this.width = 840;
+            this.showLegend = false;
+        }
+        this.setState({});
+    }
+
     
   
 
     render(){
-
         var data=[];
         var count=0;
         var height=50;
@@ -54,7 +79,7 @@ class ChartComponent extends React.Component{
         x = function(d) {
             return +d;
         },
-        width=1000,
+        width=this.width,
         legendClassName = "test-legend",
         legendPosition = 'right',
         yScale = "ordinal",
@@ -63,15 +88,21 @@ class ChartComponent extends React.Component{
         xOrient = 'top',
         xTickOrient = 'top',
         yOrient="right",
+        legendOffset=1000,
         xTickFormat = d3.format("%"),
         xTicks=[2,"%"],
+        showLegend=false,
         yTicks=[y,"%"];
         // var orderData=this.props.orders[0];
         //   console.log(orderData);   
             return (
-            
-                <div className="container">
+                
+                <div>
                     <h1 className="text-center">Order Execution Status</h1>
+                <WindowResizeListener onResize={this.changeWidth.bind(this)}/>
+                    
+                    <div className="container col-xs-12">
+                <div className="col-xs-8">
                     <BarStackHorizontalChart
                         title=''
                         data={data}
@@ -90,7 +121,20 @@ class ChartComponent extends React.Component{
                         xTickFormat={xTickFormat}
                         legendClassName = {legendClassName}
                         legendPosition= {legendPosition}
-                    />
+                        showLegend={showLegend}
+                    /></div>
+                    <div className="col-xs-4">
+                    <Legend
+                        width= {150}
+                        height= {150}
+                        legendClassName= {legendClassName}
+                        legendPosition= {legendPosition}
+                        legendOffset= {legendOffset}
+                        chartSeries = {chartSeries}
+
+                    />
+                    </div> 
+                    </div>
                 </div>
         )
     }
