0844e82d0d816273f78ffb92850fc11c926700b5
Utkarsh <utkarsh.sriv12@gmail.com>
2017-04-13 16:57:44 +0530

    pagination

--- client/components/TraderDesktop/table.component.js
+++ client/components/TraderDesktop/table.component.js
@@ -26,16 +26,43 @@ createCustomModalHeader(onClose, onSave) {
   
 }
 
+  renderShowsTotal(start, to, total) {
+    return (
+      <p style={ { color: 'blue' } }>
+        From { start } to { to }, totals is { total }&nbsp;&nbsp;(its a customize text)
+      </p>
+    );
+  }
+
     render(){
 
-        const options = {
-            insertModalHeader: this.createCustomModalHeader
-            };
-
+                const options = {
+            insertModalHeader: this.createCustomModalHeader,                    
+      page: 2,  // which page you want to show as default
+      sizePerPageList: [ {
+        text: '5', value: 5
+      }, {
+        text: '10', value: 10
+      }, {
+        text: 'All', value: this.props.orders.length
+      } ], // you can change the dropdown list for size per page
+      sizePerPage: 5,  // which size per page you want to locate as default
+      pageStartIndex: 0, // where to start counting the pages
+      paginationSize: 3,  // the pagination bar size.
+      prePage: 'Prev', // Previous page button text
+      nextPage: 'Next', // Next page button text
+      firstPage: 'First', // First page button text
+      lastPage: 'Last', // Last page button text
+      paginationShowsTotal: this.renderShowsTotal,  // Accept bool or function
+      paginationPosition: 'top'  // default is bottom, top and both is all available
+      // hideSizePerPage: true > You can hide the dropdown for sizePerPage
+      // alwaysShowAllBtns: true // Always show next and previous button
+      // withFirstAndLast: false > Hide the going to First and Last page button
+    };
         return (
            <div >
             <div className="col-xs-12 hidden-xs hidden-sm">
-              <BootstrapTable data={this.props.orders} options={options}  striped hover>
+              <BootstrapTable data={this.props.orders} options={options}  pagination={ true } striped hover>
                 <TableHeaderColumn dataField='id' isKey dataAlign="center">ID</TableHeaderColumn>
                 <TableHeaderColumn dataField='creationTime' dataAlign="center">Creation Time</TableHeaderColumn>
                 <TableHeaderColumn dataField='side' dataAlign="center">Side</TableHeaderColumn>
