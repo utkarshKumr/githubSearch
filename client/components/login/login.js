357e2e2f530277017eb05045e2185a40079f3d6b
RISHI <rishiagarwal95@gmail.com>
2017-04-18 16:59:55 +0530

    Login Updated

--- client/components/login/login.js
+++ client/components/login/login.js
@@ -10,6 +10,7 @@ import MenuItem from 'material-ui/MenuItem';
 import SelectField from 'material-ui/SelectField';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import CircularProgress from 'material-ui/CircularProgress';
 
 class LoginComponent extends React.Component{
 
@@ -17,6 +18,9 @@ constructor(props)
 {
     super(props);
     this.state = {
+            errorText:"",
+            noview:0,
+            view:0,
             value:0,
             id : 'AM',
             name : 'Amadeus Mozart'
@@ -63,16 +67,16 @@ constructor(props)
     //     cookie.save('id', this.state.id, { path: '/' });
     // }
        firebaseAuth(){
+         this.setState({view:1});
+         this.setState({noview:1});
 
         console.log('firebaseAuth called');
         var name=this.state.name;
         var id=this.state.id;
         var removeSpace=new RegExp(' ','g');
-
         var email=id.toLowerCase().concat('@gmail.com');
-        var password=this.refs.password.getValue();//name.replace(removeSpace,'').toLowerCase();
+        var password=this.refs.password.getValue();
         console.log(email,password);
-
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(()=>{
                 console.log('authenticated');
@@ -80,23 +84,45 @@ constructor(props)
                 console.log(this.state.id);
                 browserHistory.push(`/view/${this.state.id}`);
             })
-            .catch(function(error) {
+
+            .catch((error)=> {
             // Handle Errors here.
+            this.setState({view:0});
+            this.setState({errorText:"Enter Valid Password"});
             var errorCode = error.code;
             var errorMessage = error.message;
             // ...
         });
-    }
+
+  }
+
 
 
     render(){
+      if(this.state.noview==1)
+      {
+      if (this.state.view == 1) {
+        console.log(this.state.view);
+        document.getElementById("progressBar").style.visibility = "visible";
+        document.getElementById("poweredBar").style.marginTop = "48px";
+        document.getElementById("poweredBar").style.height = "11%";
+      }
+      else if (this.state.view == 0) {
+            console.log(this.state.view);
+            console.log(document.getElementById("progressBar"));
+          document.getElementById("progressBar").style.visibility = "hidden";
+          console.log(document.getElementById("progressBar"));
+    }
+}
       const style = {
         height: 500,
-        width: 500,
         margin: 20,
         textAlign: 'center',
         display: 'inline-block',
-        opacity: 0.8
+        position:'relative',
+        width:'40%',
+        width:'squeeze'
+
       }
       const hstyle=
       {   textAlign:'center',
@@ -104,18 +130,28 @@ constructor(props)
         opacity: 0.7,
         padding: 30,
         backgroundColor: "black",
-        color:"white"
+        color:"white",
+        fontWeight:900
       }
       const powered={
-         padding: 10,
-         opacity: 0.7,
-         marginTop: 120,
-         lineHeight: 3,
-         backgroundColor: "black",
-         color:"white"
+        padding: 10,
+        opacity: 0.7,
+        marginTop:70,
+        width:'100%',
+        height:'12%',
+        position:'absolute',
+        lineHeight: 3,
+        backgroundColor: "black",
+        color:"white"
 
       }
 
+      const pstyle = {
+          visibility:'hidden'
+            }
+            const lstyle = {
+                fontWeight:900
+                  }
       const styles = {
         margin: 12,
         backgroundColor:"#FFCDD2",
@@ -123,28 +159,29 @@ constructor(props)
       }
         return (
 
-        <div className="login container">
+        <div className="container">
             <center>
-            <Paper style={style} zDepth={5} rounded={false}>
+            <Paper className="login " style={style} zDepth={5} rounded={false}>
             <div>
               <h1 style={hstyle}>Trader Login</h1>
             </div>
             <br/>
             <form className="form-horizontal">
-            <SelectField maxHeight={300} value={this.state.id} onChange={this.handleChange}>
+            <SelectField maxHeight={300} value={this.state.id} onChange={this.handleChange} labelStyle={lstyle}>
             {this.props.traders.map((item,index)=>
             <MenuItem value={item.id} key={index} primaryText={item.name}/>
             )}
-            </SelectField>
-            <TextField hintText="Password Field" floatingLabelText="Password" ref="password" type="password"/>
+            </SelectField><br/>
+            <TextField hintText="Password Field" errorText={this.state.errorText} floatingLabelText="Password" ref='password' type="password"/>
             <br/>
             {/*<Link to={`/view/${this.state.id}`} onClick={this.auth.bind(this)}>
             <input type="button" className="btn btn-primary  pull-right" value="Login"></input>
             </Link>*/}
             <RaisedButton label="Login" primary={true} style={styles}  onClick={this.firebaseAuth.bind(this)}/>
             </form>
-            <div>
-                <h5 style={powered}>Powered by Prime</h5>
+            <CircularProgress style={pstyle} id="progressBar"/>
+            <div style={powered} id="poweredBar">
+                <h5 >Powered by Sapient</h5>
               </div>
             </Paper>
             </center>
